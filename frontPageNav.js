724f52d1d4e7955721448f7d960b5510c356447f
Gabriel The Great <75393160+gabisavi727@users.noreply.github.com>
2021-10-11 19:30:31 -0700

    Update frontPageNav.js

--- frontPageNav.js
+++ frontPageNav.js
@@ -3,7 +3,13 @@ function goToMovieAgree() {
 }
 
 const movieNavCardBtn = document.getElementById('movieBtn')
+const tvShowNavCardBtn
+
 
 movieNavCardBtn.addEventListener("click", () => {
     window.location.assign("/movie.html")
+})
+
+tvShowNavCardBtn.addEventListener("click", () => {
+    window.location.assign("/tvShows.html")
 })
